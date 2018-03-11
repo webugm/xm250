9702fb4eee20fcaf0610aa13e04aa9b91ce6b4a1
webugm <tawan158@gmail.com>
2018-03-11 23:05:24 +0800

    fix index.php

--- index.php
+++ index.php
@@ -4,16 +4,7 @@ include_once "header.php";
 $xoopsOption['template_main'] = "demo_index.tpl";
 include_once XOOPS_ROOT_PATH . "/header.php";
 
-/*-----------function區--------------*/
 
-//顯示預設頁面內容
-function show_content()
-{
-    global $xoopsTpl;
-
-    $main = "模組開發中";
-    $xoopsTpl->assign('content', $main);
-}
 
 /*-----------執行動作判斷區----------*/
 include_once $GLOBALS['xoops']->path('/modules/system/include/functions.php');
@@ -22,15 +13,24 @@ $op = system_CleanVars($_REQUEST, 'op', '', 'string');
 
 switch ($op) {
 
-    // case "xxx":
-    // xxx();
-    // header("location:{$_SERVER['PHP_SELF']}");
-    // exit;
+  // case "xxx":
+  // xxx();
+  // header("location:{$_SERVER['PHP_SELF']}");
+  // exit;
 
-    default:
-        show_content();
-        break;
+  default:
+    show_content();
+    break;
 }
 
 /*-----------秀出結果區--------------*/
 include_once XOOPS_ROOT_PATH . '/footer.php';
+
+/*-----------function區--------------*/
+
+//顯示預設頁面內容
+function show_content(){
+  global $xoopsTpl;
+  $main = "模組開發中";
+  $xoopsTpl->assign('content', $main);
+}
