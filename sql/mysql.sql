2eaed6019fa6c527a6fe1770375094227f9367c2
webugm <tawan158@gmail.com>
2018-05-09 14:59:22 +0800

    sql/mysql.sql

---
+++ sql/mysql.sql
@@ -0,0 +1,17 @@
+-- 1
+CREATE TABLE `模組目錄_files_center` (
+  `files_sn` smallint(5) unsigned NOT NULL AUTO_INCREMENT COMMENT '檔案流水號',
+  `col_name` varchar(255) NOT NULL default '' COMMENT '欄位名稱',
+  `col_sn` smallint(5) unsigned NOT NULL default 0 COMMENT '欄位編號',
+  `sort` smallint(5) unsigned NOT NULL default 0 COMMENT '排序',
+  `kind` enum('img','file') NOT NULL default 'img' COMMENT '檔案種類',
+  `file_name` varchar(255) NOT NULL default '' COMMENT '檔案名稱',
+  `file_type` varchar(255) NOT NULL default '' COMMENT '檔案類型',
+  `file_size` int(10) unsigned NOT NULL default 0 COMMENT '檔案大小',
+  `description` text  COMMENT '檔案說明',
+  `counter` mediumint(8) unsigned NOT NULL default 0 COMMENT '下載人次',
+  `original_filename` varchar(255) NOT NULL default '' COMMENT '檔案名稱',
+  `hash_filename` varchar(255) NOT NULL default '' COMMENT '加密檔案名稱',
+  `sub_dir` varchar(255) NOT NULL default '' COMMENT '檔案子路徑',
+  PRIMARY KEY (`files_sn`)
+) ENGINE=MyISAM;
