3513fbb4ccfd6f4c642e7529dc047b1f17517bf7
webugm <tawan158@gmail.com>
2018-05-09 14:10:16 +0800

    onInstrall.php

--- include/onInstall.php
+++ include/onInstall.php
@@ -1,22 +1,29 @@
 <?php
 /*
+include_once XOOPS_ROOT_PATH . "/modules/ugm_tools2/InstallFunction.php";
 function xoops_module_install_模組目錄(&$module) {
 
-mk_dir(XOOPS_ROOT_PATH."/uploads/模組目錄");
+  #安裝
+  go_install();
+  #更新
+  go_update(); 
+
+  return true;
 
 return true;
 }
 
-//建立目錄
-function mk_dir($dir=""){
-//若無目錄名稱秀出警告訊息
-if(empty($dir))return;
-//若目錄不存在的話建立目錄
-if (!is_dir($dir)) {
-umask(000);
-//若建立失敗秀出警告訊息
-mkdir($dir, 0777);
+//安裝
+function go_install() {
+	global $xoopsDB;
+	#建立資料夾	
+  mk_dir(XOOPS_ROOT_PATH . "/uploads/模組目錄");
+	return true;
 }
+
+//更新
+function go_update() {
+  global $xoopsDB;
 }
  */
 ?>
